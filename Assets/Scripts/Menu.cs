80e61c8a6fcb313ba62525c26dc9a3c9e45b90a5
Nocty68 <annabelle.n68@gmail.com>
2026-03-21 22:07:32 +0100

    time starts again if you go back to menu and restart

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,6 +8,7 @@ public class Menu : MonoBehaviour
 
     public void LoadGame() {
         //pour le bouton play, lance la scene du jeu
+        Time.timeScale=1;
         SceneManager.LoadScene("1stBuild");
     }
 
