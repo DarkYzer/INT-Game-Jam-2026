96c39d9fcb89c89b1c23519fb888bedb6b592560
DarkYzer <darkyzer337@gmail.com>
2026-03-22 12:55:52 +0100

    o

--- Assets/Scripts/Objects/CardBoardScript.cs
+++ Assets/Scripts/Objects/CardBoardScript.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class CardBoardScript : ObjectScript
 {
     [SerializeField] int maxHits = 5;
+    [SerializeField] ParticleSystem explosion;
     RaycastHit[] hits;
     [SerializeField] float explosionRange = 1f;
     [SerializeField] float explosionStrength = 10f;
     public override void fakeUpdate()
     {
+        Debug.Log($"fakeUpdate: {hits.Length/2}");
         hits = Physics.RaycastAll(transform.position, Vector3.up, Mathf.Infinity);
         if (hits.Length/2 > maxHits)
             exploding();
@@ -17,9 +20,6 @@ public class CardBoardScript : ObjectScript
 
     private void exploding()
     {
-        Destroy(gameObject);
-        DragAndDropController.Instance.hasBeenPlaced.Remove(transform);
-        // Créer particule
         // mouvement d'expolsion
         RaycastHit[] hitsRange;
         hitsRange = Physics.SphereCastAll(transform.position, explosionRange, transform.up);
@@ -33,7 +33,11 @@ public class CardBoardScript : ObjectScript
                 Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
                 rb.AddForce(speed * explosionStrength, ForceMode.Impulse);
             }
-        }
+        };
+        Debug.Log($"Marcon Explosion!!"); 
+        explosion.Play();
+        Destroy(gameObject);
+        DragAndDropController.Instance.hasBeenPlaced.Remove(transform);
     }
 
 }
