01747151fb24ec333370240122f122d8021415d5
DarkYzer <darkyzer337@gmail.com>
2026-03-22 16:41:29 +0100

    Update Menu.cs

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using UnityEditor.AdaptivePerformance.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,10 +8,17 @@ public class Menu : MonoBehaviour
 
     [SerializeField] GameObject Settings;
 
+    IEnumerator Again()
+    {
+        yield return new WaitForSecondsRealtime(.5f);
+        SceneManager.LoadScene("1stBuild");
+    }
+
     public void LoadGame() {
         //pour le bouton play, lance la scene du jeu
         Time.timeScale=1;
         SceneManager.LoadScene("1stBuild");
+        StartCoroutine(Again());
     }
 
     public void ExitGame(){
